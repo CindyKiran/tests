8f270ac49e8a4d499ae4790e4f4834f3d526dc57
Cindy Yap <cindyyap@eleven.nl>
2020-11-27 16:01:15 +0100

    singleton pattern update

--- src/com/cindyk/test/designpatterns/singleton/SingletonDemo.java
+++ src/com/cindyk/test/designpatterns/singleton/SingletonDemo.java
@@ -1,20 +1,36 @@
 package com.cindyk.test.designpatterns.singleton;
 
+/**
+ * Two different stores, having shared same pollyJuice storage
+ * Updating the pollyjuice quantity on one instance, will also affect the quantity of the other instance
+ */
 public class SingletonDemo {
 	public static void main(String[] args) {
-		PollyJuiceSupply pollyJuiceSupply = PollyJuiceSupply.getInstance();
-		pollyJuiceSupply.addSupply(50);
-		System.out.println(pollyJuiceSupply.getStorage()); //50
+		StoreFred storeFred = new StoreFred();
+		StoreGeorge storeGeorge = new StoreGeorge();
 
-		//create new object
-		PollyJuiceSupply pollyJuiceSupply2 = PollyJuiceSupply.getInstance();
-		//not added anything
-		System.out.println(pollyJuiceSupply2.getStorage()); //50
+		storeFred.getPollyJuiceSupply().addSupply(50);
+		storeGeorge.getPollyJuiceSupply().removeSupply(10);
 
-		pollyJuiceSupply2.removeSupply(10);
-		System.out.println(pollyJuiceSupply2.getStorage()); //40
-		System.out.println(pollyJuiceSupply.getStorage()); //40
-
+		System.out.println(storeFred.getPollyJuiceSupply().getStorage()); //40
+		System.out.println(storeGeorge.getPollyJuiceSupply().getStorage()); //also 40
 	}
 
+}
+
+
+class StoreFred{
+	private final PollyJuiceSupply pollyJuiceSupply = PollyJuiceSupply.getInstance();
+
+	public PollyJuiceSupply getPollyJuiceSupply() {
+		return pollyJuiceSupply;
+	}
+}
+
+class StoreGeorge{
+	private final PollyJuiceSupply pollyJuiceSupply = PollyJuiceSupply.getInstance();
+
+	public PollyJuiceSupply getPollyJuiceSupply() {
+		return pollyJuiceSupply;
+	}
 }
